ff59fcf46650b0c24132c343d2c5d1777c721e3c
Shaun Martin <shaunm@airg.com>
2014-06-06 21:50:12 +0000

    Fix test for beaver conf file

--- spec/classes/beaver_config_spec.rb
+++ spec/classes/beaver_config_spec.rb
@@ -4,7 +4,7 @@ describe 'beaver', :type => :class do
   let(:facts) { { :concat_basedir => '/var/lib/puppet/concat' } }
 
   context 'config' do
-    it { should contain_file('/etc/beaver.conf') }
+    it { should contain_file('/etc/beaver/beaver.conf') }
     pending "it should test the contents"
   end
 
